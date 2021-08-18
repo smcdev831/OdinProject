777ce0d8f1e71ca6520ed987d6487c90b7d63250
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-08-18 13:45:35 -0500

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,12 @@
+let admin, name;
+name = "John";
+admin = name;
+alert(admin);
+
+
+let ourPlanetName = "Earth"
+let currentUserName = "RocketMan"
+
+
+const BIRTHDAY = '18.04.1982';
+const age = someCode(BIRTHDAY);
