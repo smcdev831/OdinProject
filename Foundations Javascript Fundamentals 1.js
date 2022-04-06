88538d4f464c840cf06f473e757b583dac761ac3
smcdev <smcdev@outlook.com>
2022-04-05 22:04:11 -0500

    Adding Odin Project JS Fundamentals 1

---
+++ Foundations Javascript Fundamentals 1.js
@@ -0,0 +1,18 @@
+console.log(23 + 97);
+console.log(1+2+3+4+5+6);
+console.log((4 + 6 + 9) / 77);
+
+let a = 10
+console.log(a);
+console.log(9 * a);
+
+let b = 7 * a
+console.log(b);
+
+const max = 57
+
+let actual = max - 13
+console.log(actual);
+
+let percentage = actual / max
+console.log(percentage);
